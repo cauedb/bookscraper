97188f09ac789da6a3a7ffeef551bb56bddee86f
Carlos Brida <carloscauedb@gmail.com>
2026-03-09 14:46:09 -0300

    add draft post /scrape

---
+++ bookscraper-ms/bookscraper-ms-webapi/Controllers/ScraperController.cs
@@ -0,0 +1,32 @@
+using BookScraper.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bookscraper_ms_webapi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ScraperController : ControllerBase
+{
+    private readonly IScraperService _scraperService;
+    private readonly ILogger<ScraperController> _logger;
+
+    public ScraperController(IScraperService scraperService, ILogger<ScraperController> logger)
+    {
+        _scraperService = scraperService;
+        _logger = logger;
+    }
+
+    [HttpPost("/scrape")]
+    public async Task<IActionResult> Scrape()
+    {
+        _logger.LogInformation("Requisição POST /scrape recebida.");
+
+        var pageSource = await _scraperService.ScrapePageSourceAsync();
+
+        return Ok(new
+        {
+            message = "Scraping concluído com sucesso.",
+            htmlLength = pageSource.Length
+        });
+    }
+}
--- bookscraper-ms/bookscraper-ms-webapi/Program.cs
+++ bookscraper-ms/bookscraper-ms-webapi/Program.cs
@@ -1,23 +1,23 @@
+using BookScraper.Domain.Interfaces;
+using BookScraper.Infrastructure.Scraping;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddHealthChecks();
+builder.Services.AddScoped<IScraperService, BookScraperService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
+app.MapHealthChecks("/health");
 
 app.Run();
