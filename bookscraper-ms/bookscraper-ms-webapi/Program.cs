34c4c8141a822a461691fb54be0d5a2ea6abbbdc
Carlos Brida <carloscauedb@gmail.com>
2026-03-10 16:56:02 -0300

    fix cors

--- bookscraper-ms/bookscraper-ms-webapi/Program.cs
+++ bookscraper-ms/bookscraper-ms-webapi/Program.cs
@@ -3,6 +3,17 @@ using BookScraper.Infrastructure.Scraping;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>()
+    ?? ["http://localhost:3000", "http://localhost:5173"];
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+        policy.WithOrigins(allowedOrigins)
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
+
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddHealthChecks();
@@ -16,6 +27,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors();
 app.UseAuthorization();
 app.MapControllers();
 app.MapHealthChecks("/health");
